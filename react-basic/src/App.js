bc585b7ef922a7547c17f80a5e98ece01e049307
liu-dejin <liudejin@kis2.cn>
2025-12-09 22:54:52 +0800

    feat: 条件渲染 逻辑与和三元

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,24 +1,12 @@
-const list = [{
-  id:1,
-  name:'ldj',
-  age:18
-},{
-  id:2,
-  name:'ldj2',
-  age:19
-},{
-  id:3,
-  name:'ldj3',
-  age:20
-}]
+const isLogin = false
 function App() {
   return (
     <div className="App">   
-    {/* 渲染列表 */}
-    {/* map循环那个结构就return那个接口 需要加key */}
-    <ul>
-      {list.map(item=><li key={item.id}>{item.name}</li>)}
-    </ul>
+    {/* 条件渲染 */}
+    {/* 逻辑与&& */}
+    {isLogin && <span>登录成功</span>}
+    {/* 三元运算符 */}
+    {isLogin ? <span>登录成功</span> : <span>loading...</span>}
     </div>
   );
 }
