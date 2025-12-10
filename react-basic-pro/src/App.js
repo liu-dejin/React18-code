d8dcbf70c20e448cb9a5c0e28e31fd2298011574
liu-dejin <liudejin@kis2.cn>
2025-12-10 19:51:42 +0800

    feat: b站评论区案例

---
+++ react-basic-pro/src/App.js
@@ -0,0 +1,199 @@
+import { useState } from "react";
+import "./App.scss";
+import avatar from "./images/bozai.png";
+import _ from "lodash";
+
+/**
+ * 评论列表的渲染和操作
+ *
+ * 1. 根据状态渲染评论列表
+ * 2. 删除评论
+ */
+
+// 评论列表数据
+const defaultList = [
+  {
+    // 评论id
+    rpid: 3,
+    // 用户信息
+    user: {
+      uid: "13258165",
+      avatar: "",
+      uname: "周杰伦",
+    },
+    // 评论内容
+    content: "哎哟，不错哦",
+    // 评论时间
+    ctime: "10-18 08:15",
+    like: 88,
+  },
+  {
+    rpid: 2,
+    user: {
+      uid: "36080105",
+      avatar: "",
+      uname: "许嵩",
+    },
+    content: "我寻你千百度 日出到迟暮",
+    ctime: "11-13 11:29",
+    like: 88,
+  },
+  {
+    rpid: 1,
+    user: {
+      uid: "30009257",
+      avatar,
+      uname: "黑马前端",
+    },
+    content: "学前端就来黑马",
+    ctime: "10-19 09:00",
+    like: 66,
+  },
+];
+// 当前登录用户信息
+const user = {
+  // 用户id
+  uid: "30009257",
+  // 用户头像
+  avatar,
+  // 用户昵称
+  uname: "黑马前端",
+};
+
+/**
+ * 导航 Tab 的渲染和操作
+ *
+ * 1. 渲染导航 Tab 和高亮
+ * 2. 评论列表排序
+ *  最热 => 喜欢数量降序
+ *  最新 => 创建时间降序
+ */
+
+// 导航 Tab 数组
+const tabs = [
+  { type: "hot", text: "最热" },
+  { type: "time", text: "最新" },
+];
+const App = () => {
+  // 1.使用useState维护列表
+  const [list, setList] = useState(_.orderBy(defaultList, "like", "desc") );
+  // 删除逻辑
+  const [activeType, setActiveType] = useState("hot");
+  const handleDelete = (id) => {
+    console.log(id);
+    // 对defaultList过滤
+    setList(list.filter((item) => item.rpid !== id));
+  };
+  // tab切换高亮
+  // 1.点击谁就把谁的type记录下来
+  // 2.记录的type和每一项匹配 控制类名显示
+  const handleTableChange = (type) => {
+    console.log(type);
+    setActiveType(type);
+    // 基于列表排序
+    if (type === "hot") {
+      // 根据点赞数排序
+      // lodash
+      setList(_.orderBy(list, "like", "desc"));
+    } else {
+      // 根据创建时间排序
+      setList(_.orderBy(list, "ctime", "desc"));
+    }
+  };
+  return (
+    <div className="app">
+      {/* 导航 Tab */}
+      <div className="reply-navigation">
+        <ul className="nav-bar">
+          <li className="nav-title">
+            <span className="nav-title-text">评论</span>
+            {/* 评论数量 */}
+            <span className="total-reply">{10}</span>
+          </li>
+          <li className="nav-sort">
+            {/* 高亮类名： active */}
+            {tabs.map((item) => (
+              <span
+                onClick={() => handleTableChange(item.type)}
+                key={item.type}
+                className={`nav-item ${
+                  activeType === item.type ? "active" : ""
+                }`}
+              >
+                {item.text}
+              </span>
+            ))}
+          </li>
+        </ul>
+      </div>
+
+      <div className="reply-wrap">
+        {/* 发表评论 */}
+        <div className="box-normal">
+          {/* 当前用户头像 */}
+          <div className="reply-box-avatar">
+            <div className="bili-avatar">
+              <img className="bili-avatar-img" src={avatar} alt="用户头像" />
+            </div>
+          </div>
+          <div className="reply-box-wrap">
+            {/* 评论框 */}
+            <textarea
+              className="reply-box-textarea"
+              placeholder="发一条友善的评论"
+            />
+            {/* 发布按钮 */}
+            <div className="reply-box-send">
+              <div className="send-text">发布</div>
+            </div>
+          </div>
+        </div>
+        {/* 评论列表 */}
+        <div className="reply-list">
+          {/* 评论项 */}
+          {list.map((item) => (
+            <div key={item.rpid} className="reply-item">
+              {/* 头像 */}
+              <div className="root-reply-avatar">
+                <div className="bili-avatar">
+                  <img
+                    className="bili-avatar-img"
+                    alt=""
+                    src={item.user.avatar}
+                  />
+                </div>
+              </div>
+
+              <div className="content-wrap">
+                {/* 用户名 */}
+                <div className="user-info">
+                  <div className="user-name">{item.user.uname}</div>
+                </div>
+                {/* 评论内容 */}
+                <div className="root-reply">
+                  <span className="reply-content">{item.content}</span>
+                  <div className="reply-info">
+                    {/* 评论时间 */}
+                    <span className="reply-time">{item.ctime}</span>
+                    {/* 评论数量 */}
+                    <span className="reply-time">点赞数:{item.like}</span>
+                    {user.uid === item.user.uid && (
+                      <span
+                        className="delete-btn"
+                        onClick={() => handleDelete(item.rpid)}
+                      >
+                        删除
+                      </span>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,12 +1,7 @@
-import './index.css'
-const style = {color:"pink",fontSize:"20px"}
+
 function App() {
   return (
-    <div className="App">  
-     {/*行内样式控制  */}
-     <span style={style}>this is span</span>
-     {/* class类名控制 */}
-     <span className="foo">this is span</span>
+    <div className="App">   
     </div>
   );
 }
