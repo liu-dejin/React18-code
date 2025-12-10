82e864d8b9665be4544f50f52753c7376ff40383
liu-dejin <liudejin@kis2.cn>
2025-12-10 18:49:58 +0800

    feat:组件

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,16 +1,7 @@
-// 1.定义组件
-const Button = () => {
-  // 业务逻辑 组件逻辑
-  return <button>click me!</button>;
-}
+
 function App() {
   return (
-    <div className="App">
-      {/* 2.使用组件 */}
-      {/* 自闭和 */}
-      <Button />
-      {/* 成对标签 */}
-      <Button></Button>
+    <div className="App">   
     </div>
   );
 }
