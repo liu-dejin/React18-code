b113b0fdc64473582808130e108bd9a7a7677ea5
liu-dejin <liudejin@kis2.cn>
2025-12-10 20:06:49 +0800

    feat: 表单受控绑定

---
+++ react-basic copy/src/App.js
@@ -0,0 +1,17 @@
+// 1.声明一个react状态
+// 2.核心绑定流程
+// 1.value绑定状态
+
+import { useState } from "react";
+
+// 2.onChange e获取到数据反向修改
+function App() {
+  const [value,setValue] = useState('')
+  return (
+    <div className="App">   
+    <input value={value} onChange={(e)=>setValue(e.target.value)} type="text" />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-basic copy/src/index.js
@@ -0,0 +1,11 @@
+// 项目入口文件
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// 导入项目的根组件
+import App from './App';
+
+// 将App跟组件渲染到id为root元素中
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
