0cbdbfe702dc43aec2651639497bcd7f1351f79c
liu-dejin <liudejin@kis2.cn>
2025-12-11 21:20:07 +0800

    feat: hook函数的注意点

--- react-basic copy/src/App copy.js
+++ react-basic copy/src/App copy.js
@@ -2,16 +2,16 @@
 // 2.核心绑定流程
 // 1.value绑定状态
 
-import { useState } from "react";
+import { useState } from "react"
 
 // 2.onChange e获取到数据反向修改
-function App() {
-  const [value,setValue] = useState('')
+function App () {
+  const [value, setValue] = useState('')
   return (
-    <div className="App">   
-    <input value={value} onChange={(e)=>setValue(e.target.value)} type="text" />
+    <div className="App">
+      <input value={value} onChange={(e) => setValue(e.target.value)} type="text" />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- react-basic copy/src/App.js
+++ react-basic copy/src/App.js
@@ -1,28 +1,16 @@
-// 封装自定义hook
-// 1.声明一个use开头的函数 2.函数体内封装可复用的逻辑
-// 3.return需要的状态和回调 4.在哪个组件需要使用 就执行函数解构状态和回调
-// 自定义hook 封装状态切换
 import { useState } from "react"
-function useTaggle () {
-  // 可复用的逻辑代码
-  const [value, setValue] = useState(true)
-  const taggle = () => {
-    setValue(!value)
-  }
-  // 那些状态和回调函数在其他组件使用就要return出去
-  return {
-    value,
-    taggle
-  }
-}
-function App () {
-  const { value, taggle } = useTaggle()
-  return (
-    <div >
-      {value && <div>this is div</div>}
-      <button onClick={taggle}>taggle</button>
-    </div>
 
+// 1.组件外使用
+const [value, setValue] = useState('')
+function App () {
+  // 只能在组件顶层调用 不能嵌套到if for 其他函数中
+  if (Math.random() > 0.5) {
+    const [value, setValue] = useState('')
+  }
+  return (
+    <div className="App">
+
+    </div>
   )
 }
 
