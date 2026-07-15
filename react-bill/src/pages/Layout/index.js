2a912dbca0a29ff6025216d1e8ff79a426d11160
liudejin <liudejin@kis2.cn>
2026-07-15 15:23:10 +0800

    主题色定制化

--- react-bill/src/pages/Layout/index.js
+++ react-bill/src/pages/Layout/index.js
@@ -1,9 +1,15 @@
+import { Button } from "antd-mobile"
 import { Outlet } from "react-router-dom"
 
 const Layout = () => {
   return <div>
     <Outlet />
     Layout
+    {/* 测试全局生效样式 */}
+    <Button color="primary">全局测试</Button>
+    <div className="puple">
+      <Button color="primary">局部测试</Button>
+    </div>
   </div>
 }
 
